06730f598e95a74bae957fc5b9edb01bac5f1de9
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2022-08-11 22:34:41 +0000

    removed commented code in App component

--- src/App.jsx
+++ src/App.jsx
@@ -32,41 +32,10 @@ function App() {
       ease: "none",
       scrollTrigger: { scrub: 0.3 },
     });
-    /*
-    const element = ref.current;
-    let proxy = { skew: 0 },
-      skewSetter = gsap.quickSetter(
-        element.querySelectorAll(".skewElem"),
-        "skewY",
-        "deg"
-      ), // fast
-      clamp = gsap.utils.clamp(-20, 20); // don't let the skew go beyond 20 degrees.
-
-    ScrollTrigger.create({
-      onUpdate: (self) => {
-        let skew = clamp(self.getVelocity() / -300);
-        if (Math.abs(skew) > Math.abs(proxy.skew)) {
-          proxy.skew = skew;
-          gsap.to(proxy, {
-            skew: 0,
-            duration: 0.8,
-            ease: "power3",
-            overwrite: true,
-            onUpdate: () => skewSetter(proxy.skew),
-          });
-        }
-      },
-    });
-
-    // make the right edge "stick" to the scroll bar. force3D: true improves performance
-    gsap.set(element.querySelectorAll(".skewElem"), {
-      transformOrigin: "right center",
-      force3D: true,
-    }); */
     Events.scrollEvent.register("end", function (to, element) {
       setModalOpen(false);
     });
-
+    
     let mybutton = document.getElementById("btn-back-to-top");
     window.onscroll = function () {
       scrollFunction();
