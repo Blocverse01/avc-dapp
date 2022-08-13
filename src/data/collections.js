da15ce0889369e0dcbd53f1993bf9afde64a1c5f
Avoaja Joshua Akachi <akachijc@gmail.com>
2022-08-13 23:19:06 +0100

    change rough diamonds to ghost diamonds

--- src/data/collections.js
+++ src/data/collections.js
@@ -1,16 +1,11 @@
 import diamondHead1 from "./collectionImages/diamond-head1.png";
 import diamondHead2 from "./collectionImages/diamond-head2.png";
 import diamondHead3 from "./collectionImages/diamond-head3.png";
-import fresco1 from "./collectionImages/fresco1.png";
-import fresco2 from "./collectionImages/fresco2.png";
-import fresco3 from "./collectionImages/fresco3.png";
-import fresco4 from "./collectionImages/fresco4.png";
-import fresco5 from "./collectionImages/fresco5.png";
-import fresco6 from "./collectionImages/fresco6.png";
+
 
 export const collections = [{
     id: "leriq", title: "LeriQ", group: [{
-        name: "Rough Diamonds Collection", items: [
+        name: "Ghost Diamonds Collection", items: [
             { image: diamondHead1, title: "" }, { image: diamondHead3, title: "" },
             { image: diamondHead2, title: "" }],
     }], description1: "GRAMMY AWARD WINNING NIGERIAN SUPER PRODUCER LERIQ IS RELEASING THE MUCH-ANTICIPATED ROUGH DIAMONDS NFT COLLECTION WHICH WILL GIVE THE HOLDER ACCESS TO THE WORLD OF A MUSIC LEGEND. THE GENESIS COLLECTION WILL COMPRISE OF 3,333 NFTS THAT COMPRISE OF 13 DIFFERENT VARIATIONS WITH VARYING UTILITY, BENEFITS AND ACCESS."
@@ -18,13 +13,5 @@ export const collections = [{
     , description3: "THE NFT WILL IMMEDIATELY PROVIDE ACCESS TO THREE EXCLUSIVE SONGS FEATURING BNXN (FORMERLY BUJU), TOMI THOMAS AND LERIQ. ONLY HOLDERS OF THE NFT WILL BE ABLE TO LISTEN TO THIS MUSIC. IN THE FUTURE, NFT HOLDERS WILL ALSO BE PROVIDED WITH ELEVATED ACCESS TO PRIVATE EVENTS AND CONCERTS (IN THE REAL WORLD AND IN THE METAVERSE), MERCHANDISE, MUSIC, AIRDROPS AND FUTURE WEB 3 PROJECTS."
     , description4: "THIS COLLECTION IS THE FIRST AVC PROJECT AND THUS THE MOST RARE. AVC IS GOING TO REDEFINE THE CREATIVE SCENE ON THE CONTINENT. WE INVITE YOU TO JOIN US ON THIS JOURNEY. THIS IS A PROJECT YOU DO NOT WANT TO MISS."
     , description5: "WE ARE GRATEFUL TO ALL SUPPORTERS AND PARTNERS."
-    , description6: " JOIN THE DISCORD  HTTPS://DISCORD.COM/INVITE/AFRICANVALUABLES"
-}, {
-    id: "avc-collection", title: "AVC Collection", group: [{
-        name: "Fresco Art Collection", items: [
-            { image: fresco1, title: "" }, { image: fresco2, title: "" },
-            { image: fresco3, title: "" }, { image: fresco4, title: "" },
-            { image: fresco5, title: "" }, { image: fresco6, title: "" }
-        ]
-    }]
+    , description6: "JOIN THE DISCORD  HTTPS://DISCORD.COM/INVITE/AFRICANVALUABLES"
 }]
