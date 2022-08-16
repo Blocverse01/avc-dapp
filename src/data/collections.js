638101f132beb5c9d52c2e8ac4de1e8eb583c1fb
KingzRex <toochukwukingz6@gmail.com>
2022-08-16 01:11:38 +0000

    replace diamondHead img with diamondGhost img

--- src/data/collections.js
+++ src/data/collections.js
@@ -1,13 +1,14 @@
-import diamondHead1 from "./collectionImages/diamond-head1.png";
-import diamondHead2 from "./collectionImages/diamond-head2.png";
-import diamondHead3 from "./collectionImages/diamond-head3.png";
+// import diamondHead1 from "./collectionImages/diamond-head1.png";
+// import diamondHead2 from "./collectionImages/diamond-head2.png";
+// import diamondHead3 from "./collectionImages/diamond-head3.png";
+import diamondGhost from "./collectionImages/Diamond-Ghost-NFT.jpg";
 
 
 export const collections = [{
     id: "leriq", title: "LeriQ", group: [{
         name: "Ghost Diamonds Collection", items: [
-            { image: diamondHead1, title: "" }, { image: diamondHead3, title: "" },
-            { image: diamondHead2, title: "" }],
+            { image: diamondGhost, title: "" }, { image: diamondGhost, title: "" },
+            { image: diamondGhost, title: "" }],
     }], description1: "GRAMMY AWARD WINNING NIGERIAN SUPER PRODUCER LERIQ IS RELEASING THE MUCH-ANTICIPATED ROUGH DIAMONDS NFT COLLECTION WHICH WILL GIVE THE HOLDER ACCESS TO THE WORLD OF A MUSIC LEGEND. THE GENESIS COLLECTION WILL COMPRISE OF 3,333 NFTS THAT COMPRISE OF 13 DIFFERENT VARIATIONS WITH VARYING UTILITY, BENEFITS AND ACCESS."
     , description2: "LERIQ IS PRODUCING THESE NFTS IN COLLABORATION WITH AFRICAN VALUABLES COLLECTIVE (AVC), AFRICA’S PREMIER NFT COLLECTIVE. LERIQ HAS WORKED WITH SOME OF THE TOP ACTS IN AFRICA SUCH AS BNXN (FORMERLY BUJU), WIZKID AND LONG-TIME COLLABORATOR BURNA BOY. HAVING BEEN AT THE FOREFRONT OF THE TRANSFORMATION OF THE AFRICAN MUSIC SCENE, LERIQ SEES NFTS AS THE NATURAL STEP IN THE EVOLUTION OF THE INDUSTRY. AVC WAS BORN OUT OF THIS PRINCIPLE AND SERVES AS A VESSEL TO EDUCATE AND GUIDE CREATORS AND FANS ALIKE IN THIS JOURNEY INTO THE METAVERSE."
     , description3: "THE NFT WILL IMMEDIATELY PROVIDE ACCESS TO THREE EXCLUSIVE SONGS FEATURING BNXN (FORMERLY BUJU), TOMI THOMAS AND LERIQ. ONLY HOLDERS OF THE NFT WILL BE ABLE TO LISTEN TO THIS MUSIC. IN THE FUTURE, NFT HOLDERS WILL ALSO BE PROVIDED WITH ELEVATED ACCESS TO PRIVATE EVENTS AND CONCERTS (IN THE REAL WORLD AND IN THE METAVERSE), MERCHANDISE, MUSIC, AIRDROPS AND FUTURE WEB 3 PROJECTS."
