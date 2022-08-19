542478b908ee33007197e7766dc5394040954eb1
KingzRex <toochukwukingz6@gmail.com>
2022-08-19 11:31:14 +0000

    started team section

---
+++ src/components/Team.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import teamMembers from "../data/team";
+
+export default function team () {
+    return(
+        <section>
+            <section /* {teamMembers.map((member, index) => (
+            <Link
+            to={`/team/${member.name.split(" ").join("-").toLowerCase()}`}
+            key={index}
+            className="cursor-pointer" */
+            >
+            
+                
+
+           
+
+            </section>
+        </section>
+    )
+}
