ffd2e2d32d05498d1ecbffd1d0defa907f6d9ef9
Logan <tushlogan@gmail.com>
2022-08-22 00:32:07 +0000

    updated-faq

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -3,44 +3,33 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function Faq(props) {
-  const [open, setOpen] = useState(props.open || false);
+export default function Faq({ question, answer }) {
+  const [open, setOpen] = useState(answer.open || false);
 
-  /* const contentRef = useRef(null);
-
-  useEffect(() => {
-    contentRef.current.style.maxHeight = active
-      ? `${contentRef.current.scrollHeight}px`
-      : "0px";
-  }, [contentRef, active]);
-
-  const toggleAccordion = () => {
-    setActive(!active);
-  }; */
   return (
     <div
       onClick={() => {
         setOpen(!open);
       }}
-      className="faq py-[25px] rounded-[8px] cursor-pointer text-white px-[25px]"
+      className="faq py-[25px] glass-bg  cursor-pointer text-white px-4 md:px-[25px]"
     >
       <div
         className={`${
           open ? "pb-[25px] faq-border" : ""
         } text-[14px] flex justify-between leading-[17.3px] lg:text-[25px] lg:leading-[33.07px]`}
       >
-        {props.question}{" "}
+        {question}{" "}
         <FontAwesomeIcon
           className="text-[14px] md:text-[25px] ml-[13px]"
           icon={open ? faChevronUp : faChevronDown}
         />
       </div>
-      <div 
+      <div
         className={`${
-          open ? "h-full py-[25px]" : "h-0"
+          open ? "h-full py-[18px] md:py-[25px] " : "h-0"
         } transition-all text-[11px] overflow-y-hidden lg:text-[18px] lg:leading-[27px] duration-300`}
       >
-        {props.answer}
+        {answer}
       </div>
     </div>
   );
---
+++ src/data/faq.js
@@ -0,0 +1,41 @@
+const faqs = [
+    {
+         question:"What we do?",
+        answer:"AVC curates African NFT assets. We are a platform to manage digital content and list them for sale on leading NFT exchanges. We enable African creators to securely generate and sell NFTs."
+    },
+    {
+         question:"How AVC works",
+         answer:"AVC is an organisation that assists artists in the creation and sales of NFT assets. We educate artists on the opportunity to sell digital content as NFTs. Each creator has a profile on the AVC platform to upload their assets and generate a NFT. Through AVC the artists can list their NFTs for sale on multiple exchanges which AVC has partnered with. The artists receive their funds once the NFT is sold and can track their sales/ stats on their AVC profile."
+    },
+    {
+        question:"Why Africa focussed?",
+    answer:"We believe in the power and diversity of talent from the continent of Africa and are building a true platform for creatives by creatives. Decentralisation and web3 have enabled more quality content to be outputted from the continent and AVC are spearheadding that shift in narrrative."
+    }
+    ,
+    {
+        question:"What is curation?",
+            answer:"AVC established a curation board to carefully select projects and creatives for inclusion. This is a group of projects that provide value to communities in their art form, uniqueness, connection to the continent and initial and ongoing utility."
+    }
+    ,
+    {
+        question:"What is the curation process?",
+            answer:"Artists/creators are selected to deploy projects at the sole discretion of the AVC team. We reserve the right to curate the content on the platform without explanation. That said, no content which could be considered even remotely offensive to anyone will be considered. The onboarding process for a new project is extensive. Each project will be required to deploy on testnet and be tested thoroughly before migrating and deploying on mainnet. Once the project is ready to go live the AVC team proceed with minting on their own or partner platforms"
+    }
+    ,
+    {
+         question:"How do I apply as a creative?",
+            answer:"You may apply on our website via link. Our applications will remain open indefinitely. We expect that artists have a creative history in the music, art, sport or general creative spaces. If you plan to work in a collaborative fashion, please provide complete information for all contributors. All submitted work must be original. We do not intend to release projects that use licensed, purchased or recycled content. Please make sure you’re bringing your own unique project to AVC. "
+    }
+    ,
+    {
+         question:"As an artist, why should I choose the AVC platform??",
+            answer:"AVC was created by a creative for creatives. We understand the artist mentality and objectives and are seeking to guide them into the metaverse."
+    }
+    ,
+    {
+          question:" How does AVC make its money?",
+            answer:"AVC takes a commission on the work created in collaboration with the client. In the future AVC will seek to launch its own platform, which will be powered by its AVC tokens."
+    }
+]
+
+export default faqs;
