e2fbe7de4d2d38a075ef8cf0bb61e0880a63e707
Avoaja Joshua Akachi <akachijc@gmail.com>
2022-10-18 21:45:20 +0000

    add loading state to form

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { httpClient } from "../util/http-client";
 import Swal from "sweetalert2";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
 
 export default function Newsletter() {
   const defaultUserDetails = {
@@ -8,8 +10,10 @@ export default function Newsletter() {
     email: "",
   };
   const [userDetails, setUserDetails] = useState(defaultUserDetails);
+  const [loading, setLoading] = useState(false);
 
   const sendNewsLetter = async () => {
+    setLoading(true);
     const formID = import.meta.env.VITE_CONVERTKIT_FORM_ID;
     try {
       const { data } = await httpClient.post(`/forms/${formID}/subscribe`, {
@@ -29,15 +33,24 @@ export default function Newsletter() {
       throw new Error("Error sending confirmation email");
     } catch (err) {
       Swal.fire({
-        title: "Something happened!",
+        title: "Something went wrong!",
         text: "Please verify that your email is correct and try again.",
         icon: "info",
       });
+    } finally {
+      setLoading(false);
     }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (userDetails.name.trim() === "" || userDetails.email.trim() === "") {
+      Swal.fire({
+        title: "Something went wrong!",
+        text: "Please input your name and email.",
+        icon: "error",
+      });
+    }
     await sendNewsLetter();
   };
 
@@ -59,6 +72,8 @@ export default function Newsletter() {
             id="first-name"
             className="Home-contact-form-input"
             type="text"
+            placeholder="Your firstname"
+            required
           />
         </div>
         <div className="md:col-span-2">
@@ -72,11 +87,20 @@ export default function Newsletter() {
             name="email"
             className="Home-contact-form-input"
             type="email"
+            required
+            placeholder="Your email"
           />
         </div>
         <div className="md:col-span-2">
-          <button className="Home-contact-form-button" type="submit">
-            Subscribe!
+          <button
+            disabled={loading}
+            className="Home-contact-form-button"
+            type="submit"
+          >
+            Subscribe!{" "}
+            {loading && (
+              <FontAwesomeIcon className="ml-2" icon={faSpinner} spin />
+            )}
           </button>
         </div>
       </form>
--- src/util/http-client.js
+++ src/util/http-client.js
@@ -1,12 +1,11 @@
-import axios from 'axios';
-
+import axios from "axios";
 
 const httpClient = axios.create({
-  baseURL: `${process.env.VITE_CONVERTKIT_API_URL}`,
+  baseURL: `${import.meta.env.VITE_CONVERTKIT_API_URL}`,
 });
 
-httpClient.defaults.headers.common['Accept'] = 'application/json';
-httpClient.defaults.headers.common['Content-Type'] = 'application/json';
+httpClient.defaults.headers.common["Accept"] = "application/json";
+httpClient.defaults.headers.common["Content-Type"] = "application/json";
 httpClient.defaults.timeout = 5000;
 
-export { httpClient }; 
+export { httpClient };
