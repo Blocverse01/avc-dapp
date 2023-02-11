83ad2e90189cf984d340dd869e654dcc7d5a1f85
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-02-11 14:20:08 +0100

    Update TeamMember.jsx

--- src/components/TeamMember.jsx
+++ src/components/TeamMember.jsx
@@ -15,12 +15,12 @@ export default function TeamMember() {
         <div className="p-8 backdrop-blur-md w-fit mx-auto rounded-[14px] glass-bg">
           <img
             src={validMember.picture}
-            className="w-full h-auto md:w-[434px] md:h-[591px] object-cover"
+            className="w-full h-auto md:w-[434px] md:h-[591px] lg:w-full object-cover"
             alt={validMember.name}
           />
         </div>
 
-        <div className="w-auto xl:w-[661px] md:text-[20px] backdrop-blur-md leading-[24x] md:leading-[27px] mx-auto xl:mb-20 mb-20 xl:mt-0 mt-10 xl:text-[18px] font-[400]   text-white">
+        <div className="w-full md:text-[20px] backdrop-blur-md leading-[24x] md:leading-[27px] mx-auto xl:mb-20 mb-20 xl:mt-0 mt-10 xl:text-[18px] font-[400]   text-white">
           {validMember.description}
         </div>
       </div>
