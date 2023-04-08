8b7c3439771871f59cbdcfbca8342f347841bdb3
Avoaja Joshua Akachi <avoajajoshua@gmail.com>
2023-04-08 20:22:30 +0100

    feat: code refactor

--- src/util/get-available-NFTs.js
+++ src/util/get-available-NFTs.js
@@ -8,20 +8,20 @@ const alchemy = new Alchemy(config);
 const nftAddress = import.meta.env.VITE_NFT_ADDRESS || "";
 
 export async function getNFTs() {
-  try {
-    const nfts = await alchemy.nft.getNftsForOwner(import.meta.env.VITE_NFT_WALLET_ADDRESS);
-    const roughDiamonds = nfts.ownedNfts.filter(
-      (nft) => nft.contract.address.toLowerCase() == nftAddress.toLowerCase()
-    );
-    return roughDiamonds.map((nft) => parseInt(nft.tokenId));
-  } catch (err) {
-    throw err;
-  }
+  const nfts = await alchemy.nft.getNftsForOwner(
+    import.meta.env.VITE_NFT_WALLET_ADDRESS
+  );
+  const roughDiamonds = nfts.ownedNfts.filter(
+    (nft) => nft.contract.address.toLowerCase() == nftAddress.toLowerCase()
+  );
+  return roughDiamonds.map((nft) => parseInt(nft.tokenId));
 }
 
 export function getRandomTokens(tokenIds, number) {
   let _arr = [...tokenIds];
-  return [...Array(number)].map(() => _arr.splice(Math.floor(Math.random() * _arr.length), 1)[0]);
+  return [...Array(number)].map(
+    () => _arr.splice(Math.floor(Math.random() * _arr.length), 1)[0]
+  );
 }
 
 export async function getTokensToMint(number) {
