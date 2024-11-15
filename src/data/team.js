0f5a466c3fe0360abf059b2e0d7fa9f128ead4c5
Joshua Akachi Avoaja <avoajajoshua@gmail.com>
2024-11-15 20:05:54 +0100

    update team members

--- src/data/team.js
+++ src/data/team.js
@@ -6,17 +6,24 @@ import Folusho from "./teamImages/Folusho.jpeg";
 import Varney from "./teamImages/Varney.jpeg";
 
 const teamMembers = [
-{
-    name: "LeriQ",
-    role: "Founder",
-    picture: Leriq,
-    description:
-      "Hailing from Nigeria, LeriQ is a tech executive, music producer, sound engineer, and all-around creative who recently won a Grammy for his work on superstar Burna Boy’s latest album titled Twice As Tall. Deemed one of the Top 5 producers on the entire continent, real name Eric Isaac Utere has found his niche in curating an eclectic sound that reels in African rhythm and combines them with the pulse of electronic dance music. Growing up in Port Harcourt, Nigeria, in a household of 6 siblings (all boys), LeriQ drew a lot of his influence from his parents—with his dad being a graphic artist. With unlimited music surrounding him, LeriQ began his musical journey at the young age of 13. He fondly recalls meeting one of his dad’s friends, who was a producer. “I didn’t know that you could record and overlay, keep recording and create music.  Currently signed to Burna Boy’s Spaceship label, LeriQ is responsible for some of Burna’s biggest hits. From “Like To Party ” to “Say You Love Me” featuring Wizkid to 4 songs on Twice As Tall.  LeriQ released a producer album in 2015 titled The Lost Sounds. The album featured artists such as Illbliss, Chidimma, Burna Boy, Dammy Krane, Cassper Nyovest, Wande Coal, Seyi Shay, Wizkid, Phyno, Nneka, 2face Idibia, Efya, Bez, Naeto C, and Timaya. Other artists he has worked with include global superstars Justin Bieber and Coldplay."
-     ,
-    headline: "Founder",
-
-
-},
+    {
+        name: "Varney Blamah",
+        role: "Chief Executive Officer",
+        picture: Varney,
+        description:
+            "At 24, Varney Blamah has years of experience navigating both tech startups and the music scene. With a tech hat, he’s deep into data science, and on the music side, he’s  globetrotting as an A&R for artists worldwide. Now, as COO of African Valuable Collective, Varney focuses on bridging gaps—connecting African talent, be it in music, sports, or the arts, to opportunities both at home and abroad. It's a fusion of his tech and music worlds, creating a unique path that's very Varney."
+        ,
+        headline: "CEO",
+    },
+    {
+        name: "LeriQ",
+        role: "Founder",
+        picture: Leriq,
+        description:
+          "Hailing from Nigeria, LeriQ is a tech executive, music producer, sound engineer, and all-around creative who recently won a Grammy for his work on superstar Burna Boy’s latest album titled Twice As Tall. Deemed one of the Top 5 producers on the entire continent, real name Eric Isaac Utere has found his niche in curating an eclectic sound that reels in African rhythm and combines them with the pulse of electronic dance music. Growing up in Port Harcourt, Nigeria, in a household of 6 siblings (all boys), LeriQ drew a lot of his influence from his parents—with his dad being a graphic artist. With unlimited music surrounding him, LeriQ began his musical journey at the young age of 13. He fondly recalls meeting one of his dad’s friends, who was a producer. “I didn’t know that you could record and overlay, keep recording and create music.  Currently signed to Burna Boy’s Spaceship label, LeriQ is responsible for some of Burna’s biggest hits. From “Like To Party ” to “Say You Love Me” featuring Wizkid to 4 songs on Twice As Tall.  LeriQ released a producer album in 2015 titled The Lost Sounds. The album featured artists such as Illbliss, Chidimma, Burna Boy, Dammy Krane, Cassper Nyovest, Wande Coal, Seyi Shay, Wizkid, Phyno, Nneka, 2face Idibia, Efya, Bez, Naeto C, and Timaya. Other artists he has worked with include global superstars Justin Bieber and Coldplay."
+         ,
+        headline: "Founder",
+    },
 
 // {
 //   name: "Cairon Clarke",
@@ -69,16 +76,5 @@ const teamMembers = [
    ,
   headline: "Talent Manager",
 },
-
-{
-  name: "Varney Blamah",
-  role: "Chief Executive Officer",
-  picture: Varney,
-  description:
-    "At 24, Varney Blamah has years of experience navigating both tech startups and the music scene. With a tech hat, he’s deep into data science, and on the music side, he’s  globetrotting as an A&R for artists worldwide. Now, as COO of African Valuable Collective, Varney focuses on bridging gaps—connecting African talent, be it in music, sports, or the arts, to opportunities both at home and abroad. It's a fusion of his tech and music worlds, creating a unique path that's very Varney."
-   ,
-  headline: "CEO",
-},
-
 ]
 export default teamMembers;
